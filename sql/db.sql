d24702b5a860ea8fc507d45cf99d37013412f4d9
thu <mingfei.net@gmail.com>
2016-09-19 16:30:42 +0800

    Update

--- sql/db.sql
+++ sql/db.sql
@@ -232,27 +232,3 @@ FROM db_examination.test;
 
 SELECT *
 FROM db_examination.class_paper;
-
-SELECT
-  c1.title,
-  c2.title,
-  p.id,
-  cp.time,
-  cp.status,
-  a.username
-FROM db_examination.class_paper cp INNER JOIN db_examination.class c1
-  INNER JOIN db_examination.assistant a
-  INNER JOIN db_examination.paper p
-  INNER JOIN db_examination.course c2
-    ON cp.classId = c1.id AND cp.paperId = p.id AND cp.assistantId = a.id AND p.courseId = c2.id;
-
-
-SELECT c.title
-FROM db_examination.class_paper cp INNER JOIN db_examination.paper p INNER JOIN db_examination.course c
-    ON cp.paperId = p.id AND p.courseId = c.id
-WHERE classId = 1 AND (current_timestamp BETWEEN cp.time AND date_add(cp.time, INTERVAL p.time MINUTE));
-
-SELECT *
-FROM db_examination.test
-WHERE id = 19;
-
